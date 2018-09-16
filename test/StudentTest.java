c04efdcfa4b5c4bb29ead0c3a23d549ee379c271
Miguel Pinzon Hernandez <cmpinzonh@unal.edu.co>
2018-09-15 21:36:46 -0500

    Added bdd descriptoins and fixed a test

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * You'll find the test cases further down this document.
  */
 
 import java.util.ArrayList;
@@ -88,8 +86,28 @@ public class StudentTest {
     }
     
     /**
-     * First Test Order
+     * QA Homework by Santiago Restrepo & Miguel Pinzon 
+     * In the following test cases we'll check the behaviourof the order() method.
      */
+
+
+    /**
+    First Test Case: 
+    Scenario: Order a list of students according to the known parameters.
+    Given that: I have a list of 5 students whose information is: 
+        - 33, Rumpa, 3.68
+        - 85, Ashis, 3.84
+        - 59, Samiha, 3.75
+        - 19, Samara, 3.75
+        - 22, Fahim, 3.76
+    When: I ask the order() method do sort the students.
+    Then : The expected output of the method is:
+        - Ashis
+        - Fahim
+        - Samara
+        - Samiha
+        - Rumpa
+    */ 
     @Test
     public void order1() {
         System.out.println("order1");
@@ -113,6 +131,21 @@ public class StudentTest {
         List<Student> result = Student.order(studentList);
         assertEquals(expResult, result);
     }
+
+    /**
+    Second Test Case: 
+    Scenario: Order a list of students according to the known parameters.
+    Given that: I have a list of 3 students whose information is: 
+        - 13, Miguel, 3.13
+        - 26, Santiago, 4.23
+        - 2, Antanas, 4.78
+    When: I ask the order() method do sort the students.
+    Then : The expected output of the method is:
+        - Antanas
+        - Santiago
+        - Miguel
+    */ 
+
     @Test
     public void order2() {
         System.out.println("order2");
@@ -130,14 +163,29 @@ public class StudentTest {
         List<Student> result = Student.order(studentList);
         assertEquals(expResult, result);
     }
+
+    /**
+    Third Test Case: 
+    Scenario: Order a list of students according to the known parameters.
+    Given that: I have a list of 5 students whose information is: 
+        - 9, Falcao, 5.0
+        - 7, Bacca, 1.0
+    When: I ask the order() method do sort the students.
+    Then : The expected output of the method is:
+        - Falcao
+        - Bacca
+    */ 
     @Test
     public void order3() {
         System.out.println("order3");
-        Student student1 = new Student(11,"Falcao",5.0);
+        Student student1 = new Student(9,"Falcao",5.0);
+        Student student2 = new Student(7,"Bacca",1.0);
         List<Student> studentList = new ArrayList<Student>();
         studentList.add(student1);
+        studentList.add(student2);
         List<Student> expResult = new ArrayList<>();
         expResult.add(student1);
+        studentList.add(student2);
         List<Student> result = Student.order(studentList);
         assertEquals(expResult, result);
     }
